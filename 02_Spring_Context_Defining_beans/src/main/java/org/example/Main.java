b68644991065352043a89477ebbc00a6748db4be
NoMercy17 <antonio.stiube@gmail.com>
2026-03-29 13:46:39 +0300

    Chapter 2 finished

--- 02_Spring_Context_Defining_beans/src/main/java/org/example/Main.java
+++ 02_Spring_Context_Defining_beans/src/main/java/org/example/Main.java
@@ -2,7 +2,13 @@ package org.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+import java.util.function.Supplier;
 
 @Configuration
 class ProjectConfig{
@@ -51,6 +57,32 @@ class Parrot{
     }
 }
 
+// Using stereotype annotation
+
+@Component
+class Dog{
+    private String name;
+
+    @PostConstruct
+    public void init(){
+        this.name = "Rex";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
+
+@Configuration
+@ComponentScan(basePackages = "org.example")
+class ProjectConfigDog{
+
+}
+
+
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
@@ -63,5 +95,22 @@ public class Main {
 
         Integer i = context.getBean(Integer.class);
         System.out.println(i);
+
+        // using stereotype annotations
+        var contextDog = new AnnotationConfigApplicationContext(ProjectConfigDog.class);
+        Dog d = contextDog.getBean(Dog.class);
+        System.out.println(d.getName());
+
+
+        // Programatically adding beans
+        var contextX = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot x = new Parrot();
+        x.setName("X");
+
+        Supplier<Parrot> parrotSupplier = () -> x;
+        context.registerBean("parrot1", Parrot.class, parrotSupplier);
+
+        Parrot p1 = context.getBean("parrot1",Parrot.class);
+        System.out.println(p1.getName());
     }
 }
